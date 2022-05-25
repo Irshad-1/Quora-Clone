b25d2af7affa885a895ec263c906660eb12f6909
Md Irshad <md14015@gmail.com>
2022-05-25 20:39:36 +0530

    Landing Page Completed

--- components/navbar.js
+++ components/navbar.js
@@ -15,7 +15,7 @@ function navbar(){
             <span><img src="https://img.icons8.com/ios-glyphs/20/CCCCCC/search--v1.png"/></span>
             <input type="text" placeholder="Search Quora">
         </div>
-        <div class="bg-lightgrey">Try Quo...</div>
+        <div class="bg-lightgrey hoverEffect">Try Quo...</div>
         <a  href=""><img class="round" src="https://qsf.cf2.quoracdn.net/-4-images.new_grid.profile_default.png-26-688c79556f251aa0.png"/></a>
         <a  href=""><img src="https://img.icons8.com/ios/30/000000/globe--v1.png"/></a>
         <div><a href="">Add Question</a>
