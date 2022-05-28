7c4ebc047276603522f2874ec85b08cb0a6d435f
YASHV1752 <yashv1752@gmail.com>
2022-05-28 08:29:30 +0530

    pages linked with there icons

--- components/navbar.js
+++ components/navbar.js
@@ -4,9 +4,9 @@ function navbar(){
         <h2>Quora</h2>
     </div>
     <div id="pagelinks">
-        <div><a href=""><i class="fa-solid fa-house fa-lg"></i></a></div>
+        <div><a href="../landingPage.html"><i class="fa-solid fa-house fa-lg"></i></a></div>
         <div><a href=""><i class="fa-solid fa-list fa-lg"></i></a></div>
-        <div><a href=""><i class="fa-solid fa-pen-to-square fa-lg"></i></i></a></div>
+        <div><a href="../answer.html"><i class="fa-solid fa-pen-to-square fa-lg"></i></i></a></div>
         <div><a href="../spaces.html"><i class="fa-solid fa-people-group fa-lg"></i></a></div>
         <div><a href=""><i class="fa-solid fa-bell fa-lg "></i></a></div>
     </div>
@@ -19,7 +19,7 @@ function navbar(){
 
         <a  href="../profile.html"><img class="round" src="https://qsf.cf2.quoracdn.net/-4-images.new_grid.profile_default.png-26-688c79556f251aa0.png"/></a>
         <a  href=""><img src="https://img.icons8.com/ios/30/000000/globe--v1.png"/></a>
-        <div><a href="" class="aq">Add Question</a>
+        <div><a href="../addq.html" class="aq">Add Question</a>
             <i class="arrow down "></i></div>
     </div>
 </div>`;
